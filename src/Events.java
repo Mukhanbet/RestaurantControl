c51ae76ada1d910753b366e13af1d3afcd43e9de
Mukhanbet <nurmuhanbet2004@icloud.com>
2023-12-04 19:31:46 +0600

    Event registration completed

--- src/Events.java
+++ src/Events.java
@@ -1,16 +1,17 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class Events {
+public class Events extends Menu{
     ArrayList<String> markedDays = new ArrayList<>();
+    ArrayList<String> statusEvents = new ArrayList<>();
 
     LocalDate calendar = LocalDate.now();
 
     void displayDate () {
-        System.out.println(calendar.getYear() + "\t\t\t\t\t\t\t\t\u001B[32mCreate Events\u001B[0m\n" + calendar.getMonth() + "\n");
+        System.out.println("\u001B[97m" + calendar.getYear() + "\u001B[0m\t\t\t\t\t\t\t\t<\u001B[91mback\u001B[0m\t\u001B[94mnext\u001B[0m>\n" + "\u001B[91m" + calendar.getMonth() + "\u001B[0m\n");
         for(int i = 1; i <= calendar.lengthOfMonth(); i++) {
-            if(isDateMarked(markedDays, calendar, i)) {
-            } else if(calendar.getDayOfMonth() == i) {
+            if(isDateMarked(markedDays, statusEvents, calendar, i)) {
+            } else if(LocalDate.now().getDayOfMonth() == i && calendar.isEqual(LocalDate.now())) {
                 System.out.print("\u001B[31m" + i + "\u001B[0m" + "\t\t\t");
             } else {
                 System.out.print(i + "\t\t\t");
@@ -19,23 +20,23 @@ public class Events {
                 System.out.println("\n\n");
             }
         }
+        System.out.println("\n\t\t\t\t\t\t\t\t\t\t\t <\u001B[92mexit\u001B[0m");
     }
-    public static boolean isDateMarked(ArrayList<String> markedDays, LocalDate calendar, int calendarDay) {
+    public static boolean isDateMarked(ArrayList<String> markedDays, ArrayList<String> statusEvents, LocalDate calendar, int calendarDay) {
         int i = 0;
         while(i < markedDays.size()) {
             String thatDay = markedDays.get(i);
+            String statusEvent = statusEvents.get(i);
+            String[] splitStatus = statusEvent.split(",\\s*");
             String[] splitThatDay = thatDay.split("\\s+");
             int year = Integer.parseInt(splitThatDay[0]);
             int month = Integer.parseInt(splitThatDay[1]);
             int day = Integer.parseInt(splitThatDay[2]);
-
             if(calendarDay == day && calendar.getMonthValue() == month && calendar.getYear() == year) {
-                if(splitThatDay[3].equalsIgnoreCase("Scheduled")) {
-                    System.out.print("\u001B[34m" + calendarDay + "\u001B[0m" + "\t\t\t");
-                } else if(splitThatDay[3].equalsIgnoreCase("Cancelled")) {
-                    System.out.print("\u001B[90m" + calendarDay + "\u001B[0m" + "\t\t\t");
-                } else if(splitThatDay[3].equalsIgnoreCase("finished")){
+                if (calendar.getDayOfMonth() > calendarDay) {
                     System.out.print("\u001B[35m" + calendarDay + "\u001B[0m" + "\t\t\t");
+                } else if(splitStatus[0].equalsIgnoreCase("Scheduled")) {
+                    System.out.print("\u001B[94m" + calendarDay + "\u001B[0m" + "\t\t\t");
                 } else {
                     System.out.print(calendarDay + "\t\t\t");
                 }
--- src/Main.java
+++ src/Main.java
@@ -19,22 +19,122 @@ public class Main {
 //
 //        menu.displayMenu();
 //        menu.controlSecond.add("Hello");
+
 //        menu.controlSecond.add("Nur");
 //        menu.controlSecond.add("Welcome");
 //        menu.controlSecond.add("World");
 //        menu.controlSecond.add("Manty");
 //        menu.controlSecond.add("Boso");
 //        menu.displaySecond();
-        System.out.println("Put the date\nYear|Month|Day|Event Status");
-        String eventStatus = scanner.nextLine();
-        events.markedDays.add(eventStatus);
-        String[] splitEventStatus = eventStatus.split("\\s+");
-        int year = Integer.parseInt(splitEventStatus[0]);
-        int month = Integer.parseInt(splitEventStatus[1]);
-        int day = Integer.parseInt(splitEventStatus[2]);
+//        System.out.println("Put the date\nYear|Month|Day");
+//        String eventStatus = scanner.nextLine();
+        events.markedDays.add("2023 12 12");
+//        String status = scanner.nextLine();
+        events.statusEvents.add("scheduled,fdvsd,342,fsdc");
         events.displayDate();
-        System.out.println("\n\n\n\n");
-        events.calendar = LocalDate.of(year, month, day);
-        events.displayDate();
+        while(true) {
+            String getInput = scanner.next();
+            if(getInput.equalsIgnoreCase("next")) {
+                if(events.calendar.getMonthValue() == 12) {
+                    events.calendar = LocalDate.of(events.calendar.getYear() + 1, 1, LocalDate.now().getDayOfMonth());
+                } else {
+                    events.calendar = LocalDate.of(events.calendar.getYear(), events.calendar.getMonthValue() + 1, LocalDate.now().getDayOfMonth());
+                }
+                System.out.println("Calendar" + events.calendar);
+                events.displayDate();
+            } else if (getInput.equalsIgnoreCase("back")) {
+                if(events.calendar.getMonthValue() == 1 ) {
+                    events.calendar = LocalDate.of(events.calendar.getYear() - 1, 12, LocalDate.now().getDayOfMonth());
+                } else {
+                    events.calendar = LocalDate.of(events.calendar.getYear(), events.calendar.getMonthValue() - 1, LocalDate.now().getDayOfMonth());
+                }
+                System.out.println("Calendar" + events.calendar);
+                events.displayDate();
+            } else if (getInput.equalsIgnoreCase("exit")){
+                break;
+            } else {
+                int day = Integer.parseInt(getInput);
+                String selectedTime = events.calendar.getYear() + " " + events.calendar.getMonthValue() + " " + day;
+                if(events.markedDays.contains(selectedTime)) {
+                    int indexThatDay = events.markedDays.indexOf(selectedTime);
+                    String[] splitStatus = events.statusEvents.get(indexThatDay).split(",\\s*", 4);
+                    System.out.println("\u001B[91mCancel\u001B[0m\t\t\t\t\t\t\t\t\t\t\u001B[97mChange\u001B[0m\nStatus:\t\t\t\t|\t" + splitStatus[0] + "\nEvent types:\t\t|\t" + splitStatus[1] + "\nNumber of Visitors: |\t" + splitStatus[2] + "\nSelected menu:\t\t|\t" + splitStatus[3] + "\n\t\t\t\t\t\t\t\t\t\t\t <\u001B[92mexit\u001B[0m");
+                    getInput = scanner.next();
+                    if(getInput.equalsIgnoreCase("cancel")) {
+                        events.statusEvents.remove(indexThatDay);
+                        events.markedDays.remove(indexThatDay);
+                    } else if (getInput.equalsIgnoreCase("change")) {
+                        while(true) {
+                            System.out.println("<\u001B[92mexit\u001B[0m\t\t\tA\t\t\t\tB\t\t\t\tC\n\t\t\tEvent type | Number of visitors | Menu ");
+                            getInput = scanner.next();
+                            if(getInput.equalsIgnoreCase("A")) {
+                                System.out.println("\u001B[97mEvent type\u001B[0m\n\u001B[91m|\u001B[0m " + splitStatus[1] + " \u001B[91m|\u001B[0m\n");
+                                splitStatus[1] = changeEvent(splitStatus[1]);
+                                events.statusEvents.set(indexThatDay, splitStatus[0] + "," + splitStatus[1] + "," + splitStatus[2] + "," + splitStatus[3]);
+                            } else if (getInput.equalsIgnoreCase("B")) {
+                                System.out.println("\u001B[97mNumber of visitors\u001B[0m\n\u001B[91m|\u001B[0m " + splitStatus[2] + " \u001B[91m|\u001B[0m\n");
+                                splitStatus[2] = changeEvent(splitStatus[2]);
+                                events.statusEvents.set(indexThatDay, splitStatus[0] + "," + splitStatus[1] + "," + splitStatus[2] + "," + splitStatus[3]);
+                            } else if (getInput.equalsIgnoreCase("C")) {
+                                System.out.println("\u001B[97mMenu\u001B[0m\n\u001B[91m|\u001B[0m " + splitStatus[3] + " \u001B[91m|\u001B[0m\n");
+                                menu.displayMenu();
+                                splitStatus[3] = changeEvent(splitStatus[3]);
+                                events.statusEvents.set(indexThatDay, splitStatus[0] + "," + splitStatus[1] + "," + splitStatus[2] + "," + splitStatus[3]);
+                            }
+                            if(getInput.equalsIgnoreCase("exit")) {
+                                break;
+                            }
+                        }
+                    }
+                } else {
+                    events.markedDays.add(selectedTime);
+                    System.out.println("\u001B[97mEvent type\u001B[0m\t\tA. Свадьба\t\tB. Куран окутуу\t\tC. День рождение\n\t\t\t\tD. Той\t\tE. Индивидуальный предзаказ\t\tF. Корпоративная вечеринка" );
+                    String getEventType = scanner.next();
+                    System.out.print("\u001B[97mNumber of visitors\u001B[0m ");
+                    String getNumberOfVisitors = scanner.next();
+                    System.out.print("\u001B[97mMenu\u001B[0m \t\t\t");
+                    events.displayMenu();
+                    String getMenu = scanner.next();
+                    events.statusEvents.add("scheduled," + eventType(getEventType) + "," + getNumberOfVisitors + "," + getMenu);
+                }
+            }
+            events.displayDate();
+        }
+
+    }
+
+    public static String eventType(String getEventType) {
+        String свадьба = "A";
+        String куранОкутуу = "B";
+        String денРождения = "C";
+        String той = "D";
+        String индивидуальныйПредзаказ = "E";
+        if (getEventType.equalsIgnoreCase(свадьба)) {
+            return "свадьба";
+        } else if (getEventType.equalsIgnoreCase(куранОкутуу)) {
+            return "Куран окутуу";
+        } else if (getEventType.equalsIgnoreCase(денРождения)) {
+            return "День Рождения";
+        } else if (getEventType.equalsIgnoreCase(той)) {
+            return "Той";
+        } else if (getEventType.equalsIgnoreCase(индивидуальныйПредзаказ)) {
+            return "Индивидуальный предзаказ";
+        } else {
+            return "Корпоративная вечеринка";
+        }
+    }
+
+    public static String changeEvent (String strings) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n(\u001B[91mInstructions\u001B[0m) Write down the word(s) you want to change \u001B[91mremove\u001B[0m or \u001B[92madd\u001B[0m at the end, separated by a comma.");
+        String getInput = scanner.nextLine();
+        String[] split = getInput.split(",\\s*");
+        if(split[1].equalsIgnoreCase("add")) {
+            strings = strings + " " + split[0];
+            return strings;
+        } else {
+            strings = strings.replaceAll(split[0], "");
+            return strings;
+        }
     }
 }
